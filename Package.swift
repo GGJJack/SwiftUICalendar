64c2e5cd885a204d8debe6ba8ae4f9446193755e
ggaljjak <hyojong6367@gmail.com>
2021-10-28 11:27:53 +0900

    Add SPM
    - Add platforms target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUICalendar",
+    platforms: [
+        .iOS(v14) //, .macOS(???)
+    ]
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
