371c6be85eb621aed4807b4ef9be14dde4f98048
ggaljjak <hyojong6367@gmail.com>
2021-10-28 11:30:07 +0900

    Add SPM
    - Bug fix

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftUICalendar",
     platforms: [
-        .iOS(v14) //, .macOS(???)
+        .iOS(.v14)
+        //, .macOS(???)
     ]
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
